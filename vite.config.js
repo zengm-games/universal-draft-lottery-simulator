0d0ce162a312369ce7952d54887d4be4579fab87
Jeremy Scheff <jdscheff@gmail.com>
2022-09-18 16:48:45 -0400

    Bundle to single file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-	plugins: [preact()],
-	base: "/universal-draft-lottery-simulator/",
+	plugins: [preact(), viteSingleFile({ removeViteModuleLoader: true })],
 });
