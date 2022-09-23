c3327babcca42492c6df6b06b4e429695c9ea695
Jeremy Scheff <jdscheff@gmail.com>
2022-09-23 11:38:37 -0400

    Only inline CSS

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import preact from "@preact/preset-vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-	plugins: [preact(), viteSingleFile({ removeViteModuleLoader: true })],
+	plugins: [preact(), viteSingleFile({ inlinePattern: "**/*.css" })],
+	base: "/universal-draft-lottery-simulator/",
 });
